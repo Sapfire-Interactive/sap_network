55133b932b86ff1035106727273a263e934b1f11
Dominik Kurasbediani <dominik.kurasbediani@gmail.com>
2026-03-10 19:22:33 +0100

    Better way to handle ownership in sockets

--- include/sap_network/isocket.h
+++ include/sap_network/isocket.h
@@ -1,3 +1,4 @@
+#pragma once
 #include "sap_core/types.h"
 #include "sap_network/types.h"
 
@@ -5,10 +6,10 @@ class ISocket {
 public:
     virtual ~ISocket() = default;
 
-    virtual bool bind(u16 port) = 0;
-    virtual bool listen(i32 backlog = 128) = 0;
-    virtual bool connect(stl::string_view host, u16 port) = 0;
-    virtual ISocket* accept() = 0;
+    virtual bool bind() = 0;
+    virtual bool listen() = 0;
+    virtual bool connect() = 0;
+    virtual stl::unique_ptr<ISocket> accept() = 0;
     virtual size_t send(stl::span<const std::byte>) = 0;
     virtual size_t recv(stl::span<std::byte>) = 0;
     virtual void close() = 0;
---
+++ include/sap_network/socket_config.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "sap_core/types.h"
+
+#include <chrono>
+#include <string>
+
+namespace sap::network {
+
+struct SocketConfig {
+    std::string               host;
+    u16                       port            = 0;
+    std::chrono::milliseconds connect_timeout = std::chrono::milliseconds{0}; // 0 = no timeout (blocking)
+    i32                       listen_backlog  = 128;
+};
+
+} // namespace sap::network
