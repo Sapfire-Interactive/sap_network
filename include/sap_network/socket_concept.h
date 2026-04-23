2c9cc9e94ae5d4dd737981b190cb2d01fa9320ad
Dominik Kurasbediani <dominik.kurasbediani@gmail.com>
2026-04-23 22:44:46 +0200

    Concept fixes

--- include/sap_network/socket_concept.h
+++ include/sap_network/socket_concept.h
@@ -1,13 +1,19 @@
 #pragma once
 
+#include "sap_network/socket_config.h"
+
+#include <sap_core/stl/result.h>
+#include <sap_core/stl/string.h>
 #include <sap_core/types.h>
+
+#include <concepts>
 
 namespace sap::network {
     template <typename S>
-    concept Socket = requires(S& s, stl::span<stl::const stl::byte> out_buf, stl::span<stl::byte> in_buf) {
+    concept Socket = requires(S& s, stl::span<const stl::byte> out_buf, stl::span<stl::byte> in_buf) {
         { s.connect() } -> std::convertible_to<bool>;
-        { s.send(out_buf) } -> std::convertible_to<size_t>;
-        { s.recv(in_buf) } -> std::convertible_to<size_t>;
+        { s.send(out_buf) } -> std::convertible_to<stl::result<size_t>>;
+        { s.recv(in_buf) } -> std::convertible_to<stl::result<size_t>>;
         { s.close() } -> std::same_as<void>;
         { s.valid() } -> std::convertible_to<bool>;
         { s.config() } -> std::convertible_to<const SocketConfig&>;
